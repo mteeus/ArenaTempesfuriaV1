05fb1aa233c78f51b669be2283586c9772739b7b
Matheus Lim <mteeus@gmail.com>
2018-10-13 22:39:26 -0300

    add script bola

---
+++ Assets/Scripts/Bloco.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(gameObject);
+    }
+
+
+
+    }
+
---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour {
+
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+	
+    float hitfactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x) / racketWidth;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "racket")
+        {
+            float x = hitfactor(transform.position, col.transform.position, col.collider.bounds.size.x);
+
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+
+		
+	}
+}
