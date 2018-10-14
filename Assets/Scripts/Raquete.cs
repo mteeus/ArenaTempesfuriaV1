4806aa69c50095b62ca3e493b972762b4c2046c3
Matheus Lim <mteeus@gmail.com>
2018-10-13 21:29:19 -0300

    add Sprites and Scripts

---
+++ Assets/Scripts/Raquete.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raquete : MonoBehaviour {
+
+	public float speed;
+
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = Vector2.right * h * speed;
+		
+	}
+}
