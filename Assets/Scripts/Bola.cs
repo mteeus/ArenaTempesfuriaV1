ee4384ff6ba36770b9a004f722a5fdff99e9b272
Matheus Lima <36974935+mteeus@users.noreply.github.com>
2018-10-24 22:04:25 -0300

    Update Bola.cs

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -9,6 +9,18 @@ public class Bola : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
+		sorteio=Ramdom.Range(0,10); //mínimo e máximo do sorteio
+sorteioY=Ramdom.Range(0.02f, -0.02f); //cima ou baixo
+
+if (sorteio <5){ //se o número for menor que 5 a bola irá para a DIREITA
+rigidbody2D.AddForce (new Vector2(0.05f, sorteioY));
+
+//0.05f: força ao iniciar a bola no jogo, coordenadas X(horizontal- positivo pra direita, negativo pra esquerda) e Y(vertical).
+
+} else { //se o número for maior que 5 a bola irá para a ESQUERDA
+rigidbody2D.AddForce (new Vector2(-0.05f, sorteioY));
+	
 
         GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
     }
